232e7dab485d35afb0ab28e1515fb76c2c5125c7
Gusti Tammam <gustitammam@gmail.com>
2021-02-18 19:32:03 +0700

    [ADD] Add script to install pyenv

---
+++ pyenv-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# pyenv dependencies
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+# install pyenv
+curl https://pyenv.run | bash
+
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc
+echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
